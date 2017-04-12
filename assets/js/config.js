28c6f91371ba170928de178ac148a84a7a95292f
Artur Czartoryski <artur@czartoryski.wroclaw.pl>
2017-04-12 05:42:12 +0200

    Small changes in the GUI Exclude JS configuration from index.html

---
+++ assets/js/config.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    // Hide deselect all checkbox button
+    $('a.deselect-all').hide();
+
+    // DataTable
+    var table = $('#datatable1').DataTable();
+    
+    // Setup - add a text input to each footer cell
+    $('#datatable1 thead th').each( function () {
+        var title = $(this).text();
+        $(this).html( title+'</br><input type="search" class="form-control input-sm" style="font-size: 10px; width: 100px;" placeholder="filter..." />' );
+    } );
+    
+    // Check all checkboxes in the table after click on select-all
+    $('a.select-all').on( 'click', function (e) {
+        $(":checkbox").attr('checked', true);
+        $(this).hide();
+        $('a.deselect-all').show();
+    });
+
+    // Uncheck all checkboxes in the table after click on select-all
+    $('a.deselect-all').on( 'click', function (e) {
+        $(":checkbox").attr('checked', false);
+        $(this).hide();
+        $('a.select-all').show();
+    });
+
+    // Hide or show selected column in the table
+    $('a.toggle-vis').on( 'click', function (e) {
+        e.preventDefault();
+ 
+        // Get the column API object
+        var column = table.column( $(this).attr('data-column') );
+        if ($(this).children('span').hasClass('badge-success')) {
+            $(this).children('span').removeClass('badge-success').addClass('badge-danger');
+            $(this).children('span').children('i').removeClass('fa-plus-square').addClass('fa-minus-square');
+        } else {
+            $(this).children('span').removeClass('badge-danger').addClass('badge-success');
+            $(this).children('span').children('i').removeClass('fa-minus-square').addClass('fa-plus-square');
+        }
+        
+        // Toggle the visibility
+        column.visible( ! column.visible() );
+        } );
+
+    // Apply the search in the table columns
+    table.columns().eq( 0 ).each( function ( colIdx ) {
+        $( 'input', table.column( colIdx ).header() ).on( 'keyup change', function () {
+            table
+                .column( colIdx )
+                .search( this.value )
+                .draw();
+        } );
+        $('input', table.column(colIdx).header()).on('click', function(e) {
+        e.stopPropagation();
+        });        
+    } );
+} );
